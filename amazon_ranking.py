d8f19213412f656402436c913ecc0dce6c79b409
rohanm-cyber <rohan.m@trisa.co.in>
2026-08-04 17:56:07 +0530

    Update amazon_ranking.py

--- amazon_ranking.py
+++ amazon_ranking.py
@@ -484,7 +484,7 @@ def fetch_keywords_and_sync_results(
 # RUNNER / ENTRY POINT
 # ==========================================
 if __name__ == "__main__":
-    CREDENTIALS_JSON = "AMAZON_KEYWORD.json"
+    CREDENTIALS_JSON = "G_CREDENTIAL.json"
     SPREADSHEET_NAME = "Keywords_Research"
     INPUT_SHEET_NAME = "Keywords_input"
     OUTPUT_SHEET_NAME = "Keywords_output"
