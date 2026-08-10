ecb1c5ef342c65aa9df5813dd57d41840c700ba3
rohanm-cyber <rohan.m@trisa.co.in>
2026-08-10 11:30:02 +0530

    Update amazon_ranking.py

--- amazon_ranking.py
+++ amazon_ranking.py
@@ -73,6 +73,20 @@ class AmazonOrganicRanker:
         self.max_retries = max_retries
         self.driver: Optional[uc.Chrome] = None
 
+    def _get_free_proxies(self):
+    logger.info("Free Proxy has been downloaded!")
+    url = "https://proxyscrape.com"
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            proxies = response.text.strip().split('\r\n')
+            if len(proxies) < 5:
+                proxies = requests.get("https://githubusercontent.com", timeout=10).text.strip().split('\n')
+            return proxies
+    except Exception as e:
+        logger.error(f"Proxy error to Download: {e}")
+    return []
+
     def _init_driver(self):
         if self.driver:
             self.close()
