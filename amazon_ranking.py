3254e15e12f6f73e398faf8ed14ebc6c01cc6c57
rohanm-cyber <rohan.m@trisa.co.in>
2026-08-11 17:39:04 +0530

    Update amazon_ranking.py

--- amazon_ranking.py
+++ amazon_ranking.py
@@ -74,6 +74,7 @@ class StealthAmazonRanker:
             self.close()
 
         options = uc.ChromeOptions()
+        options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
